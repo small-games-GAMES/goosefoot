89d94df4d72c451d8b396cb2d1c16083ba112e4d
corrannolan <corrannolan3097@gmail.com>
2019-06-06 23:46:54 -0400

    small stuff but good stuff

--- Goose Foot Project/Assets/Scripts/soundManager.cs
+++ Goose Foot Project/Assets/Scripts/soundManager.cs
@@ -4,36 +4,26 @@ using UnityEngine;
 
 public class soundManager : MonoBehaviour
 {
-
     public AudioSource sauce;
     public AudioClip honk, kickSound, hitHeadSound, winSound;
 
     public void playHonk()
     {
-
         sauce.PlayOneShot(honk, Random.Range(0.9f, 1.2f));
-
     }
 
     public void playKick()
     {
-
         sauce.PlayOneShot(kickSound, Random.Range(0.9f, 1.2f));
-
     }
 
     public void playHit()
     {
-
         sauce.PlayOneShot(hitHeadSound);
-
     }
 
     public void playWin()
     {
-
         sauce.PlayOneShot(winSound);
-
     }
-
 }
